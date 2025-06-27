a21516b5cfb07c375cec35b141aa0ba20c9d539e
Omer Faruk <ozisomerfaruk@gmail.com>
2025-06-28 02:19:11 +0300

    Add files via upload

---
+++ workautomations/workautomations/AddEmployee.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace workautomations
+{
+    public partial class AddEmployee : Form
+    {
+        SqlConnection connect = new SqlConnection(@"your string");
+
+        public AddEmployee()
+        {
+            InitializeComponent();
+
+            displayEmployeeData();
+        }
+
+        public void RefreshData()
+        {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)RefreshData);
+                return;
+            }
+            displayEmployeeData();
+        }
+
+        public void displayEmployeeData()
+        {
+            EmployeeData ed = new EmployeeData();
+            List<EmployeeData> listData = ed.employeeListData();
+
+            dataGridView1.DataSource = listData;
+        }
+
+
+        private void AddEmployee_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addEmployee_id_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addEmployee_addBtn_Click(object sender, EventArgs e)
+        {
+            if (addEmployee_id.Text == ""
+               || addEmployee_fullName.Text == ""
+               || addEmployee_gender.Text == ""
+               || addEmployee_phoneNum.Text == ""
+               || addEmployee_position.Text == ""
+               || addEmployee_status.Text == ""
+               )
+            {
+                MessageBox.Show("Please fill all blank fields"
+                    , "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (connect.State == ConnectionState.Closed)
+                {
+                    try
+                    {
+                        connect.Open();
+                        string checkEmID = "SELECT COUNT(*) FROM employees WHERE employee_id = @emID AND delete_date IS NULL";
+
+                        using (SqlCommand checkEm = new SqlCommand(checkEmID, connect))
+                        {
+                            checkEm.Parameters.AddWithValue("@emID", addEmployee_id.Text.Trim());
+                            int count = (int)checkEm.ExecuteScalar();
+
+                            if (count >= 1)
+                            {
+                                MessageBox.Show(addEmployee_id.Text.Trim() + " is already taken"
+                                    , "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                            else
+                            {
+                                DateTime today = DateTime.Today;
+                                string insertData = "INSERT INTO employees " +
+                                    "(employee_id, full_name, gender, contact_number" +
+                                    ", position, salary, insert_date, status, performance) " +
+                                    "VALUES(@employeeID, @fullName, @gender, @contactNum" +
+                                    ", @position, @salary, @insertDate, @status, @performance)";
+
+
+
+                                using (SqlCommand cmd = new SqlCommand(insertData, connect))
+                                {
+                                    cmd.Parameters.AddWithValue("@employeeID", addEmployee_id.Text.Trim());
+                                    cmd.Parameters.AddWithValue("@fullName", addEmployee_fullName.Text.Trim());
+                                    cmd.Parameters.AddWithValue("@gender", addEmployee_gender.Text.Trim());
+                                    cmd.Parameters.AddWithValue("@contactNum", addEmployee_phoneNum.Text.Trim());
+                                    cmd.Parameters.AddWithValue("@position", addEmployee_position.Text.Trim());
+
+                                    cmd.Parameters.AddWithValue("@salary", 0);
+                                    cmd.Parameters.AddWithValue("@insertDate", today);
+                                    cmd.Parameters.AddWithValue("@status", addEmployee_status.Text.Trim());
+                                    cmd.Parameters.AddWithValue("@performance", emp_perf.Text.Trim());
+
+                                    cmd.ExecuteNonQuery();
+
+                                    displayEmployeeData();
+
+                                    MessageBox.Show("Added successfully!"
+                                        , "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex
+                    , "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        connect.Close();
+                    }
+                }
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex != -1)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                addEmployee_id.Text = row.Cells[0].Value.ToString();
+                addEmployee_fullName.Text = row.Cells[1].Value.ToString();
+                addEmployee_gender.Text = row.Cells[2].Value.ToString();
+                addEmployee_phoneNum.Text = row.Cells[3].Value.ToString();
+                addEmployee_position.Text = row.Cells[4].Value.ToString();
+                addEmployee_status.Text = row.Cells[6].Value.ToString();
+                emp_perf.Text = row.Cells[7].Value.ToString();
+
+            }
+        }
+
+        public void clearFields()
+        {
+            addEmployee_id.Text = "";
+            addEmployee_fullName.Text = "";
+            addEmployee_gender.SelectedIndex = -1;
+            addEmployee_phoneNum.Text = "";
+            addEmployee_position.SelectedIndex = -1;
+            addEmployee_status.SelectedIndex = -1;
+            emp_perf.SelectedIndex = -1;
+        }
+
+        private void addEmployee_updateBtn_Click(object sender, EventArgs e)
+        {
+            if (addEmployee_id.Text == ""
+               || addEmployee_fullName.Text == ""
+               || addEmployee_gender.Text == ""
+               || addEmployee_phoneNum.Text == ""
+               || addEmployee_position.Text == ""
+               || addEmployee_status.Text == ""
+              )
+            {
+                MessageBox.Show("Please fill all blank fields"
+                    , "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                DialogResult check = MessageBox.Show("Are you sure you want to UPDATE " +
+                    "Employee ID: " + addEmployee_id.Text.Trim() + "?", "Confirmation Message"
+                    , MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                if (check == DialogResult.Yes)
+                {
+                    try
+                    {
+                        connect.Open();
+                        DateTime today = DateTime.Today;
+
+                        string updateData = "UPDATE employees SET full_name = @fullName" +
+     ", gender = @gender, contact_number = @contactNum" +
+     ", position = @position, update_date = @updateDate, status = @status, performance = @performance " +
+     "WHERE employee_id = @employeeID";
+
+
+                        using (SqlCommand cmd = new SqlCommand(updateData, connect))
+                        {
+                            cmd.Parameters.AddWithValue("@fullName", addEmployee_fullName.Text.Trim());
+                            cmd.Parameters.AddWithValue("@gender", addEmployee_gender.Text.Trim());
+                            cmd.Parameters.AddWithValue("@contactNum", addEmployee_phoneNum.Text.Trim());
+                            cmd.Parameters.AddWithValue("@position", addEmployee_position.Text.Trim());
+                            cmd.Parameters.AddWithValue("@updateDate", today);
+                            cmd.Parameters.AddWithValue("@status", addEmployee_status.Text.Trim());
+                            cmd.Parameters.AddWithValue("@employeeID", addEmployee_id.Text.Trim());
+                            cmd.Parameters.AddWithValue("@performance", emp_perf.Text.Trim());
+                            cmd.ExecuteNonQuery();
+
+                            displayEmployeeData();
+
+                            MessageBox.Show("Update successfully!"
+                                , "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            clearFields();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex
+                        , "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        connect.Close();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Cancelled."
+                        , "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+
+            }
+        }
+
+        private void addEmployee_clearBtn_Click(object sender, EventArgs e)
+        {
+            clearFields();
+        }
+
+        private void addEmployee_deleteBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(addEmployee_id.Text) ||
+       string.IsNullOrWhiteSpace(addEmployee_fullName.Text) ||
+       string.IsNullOrWhiteSpace(addEmployee_gender.Text) ||
+       string.IsNullOrWhiteSpace(addEmployee_phoneNum.Text) ||
+       string.IsNullOrWhiteSpace(addEmployee_position.Text) ||
+       string.IsNullOrWhiteSpace(addEmployee_status.Text))
+            {
+                MessageBox.Show("Please fill all blank fields", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            DialogResult check = MessageBox.Show("Are you sure you want to DELETE " +
+                "Employee ID: " + addEmployee_id.Text.Trim() + "?", "Confirmation Message", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+            if (check == DialogResult.Yes)
+            {
+                try
+                {
+
+                    using (SqlConnection conn = new SqlConnection(@"Data Source=OMER\SQLEXPRESS;Initial Catalog=employeee;Integrated Security=True;")) // connectionString'i doğru şekilde ayarladığınızdan emin olun
+                    {
+                        conn.Open();
+
+
+                        DateTime today = DateTime.Today;
+
+
+                        string updateData = "UPDATE employees SET delete_date = @deleteDate WHERE employee_id = @employeeID";
+
+
+                        using (SqlCommand cmd = new SqlCommand(updateData, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@deleteDate", today);
+                            cmd.Parameters.AddWithValue("@employeeID", addEmployee_id.Text.Trim());
+
+
+                            int rowsAffected = cmd.ExecuteNonQuery();
+
+                            if (rowsAffected > 0)
+                            {
+                                displayEmployeeData();
+                                MessageBox.Show("Update successfully!", "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                                clearFields();
+                            }
+                            else
+                            {
+                                MessageBox.Show("Employee ID not found or no changes were made.", "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Cancelled.", "Information Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainForm mainForm = new MainForm();
+            mainForm.Show();
+            this.Hide();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void emp_perf_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addEmployee_status_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+        
+    
+
+
+
+
